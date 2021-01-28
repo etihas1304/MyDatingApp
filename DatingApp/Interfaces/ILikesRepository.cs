4d6e9336cc76db720611598650148a8850ffd765
eti has <etihas1304@gmail.com>
2021-01-28 20:15:23 +0200

    section 14

---
+++ DatingApp/Interfaces/ILikesRepository.cs
@@ -0,0 +1,17 @@
+﻿using DatingApp.Dtos;
+using DatingApp.Entities;
+using DatingApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);
+    }
+}
